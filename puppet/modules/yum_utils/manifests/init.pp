48beeb5fd8ee54340663db40ae7908a743f5d376
Brice Bentler <server4001@gmail.com>
2016-09-06 08:44:05 -0700

    Adds install of htpasswd

--- puppet/modules/yum_utils/manifests/init.pp
+++ puppet/modules/yum_utils/manifests/init.pp
@@ -1,9 +1,13 @@
 class yum_utils {
   package { 'yum-utils':
     ensure => 'latest',
-  }->
+  }
 
   package { 'createrepo':
+    ensure => 'latest',
+  }
+
+  package { 'httpd-tools':
     ensure => 'latest',
   }
 }
