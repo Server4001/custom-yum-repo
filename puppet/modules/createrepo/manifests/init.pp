44f2f118b3347279f45f0c0956457634a5e0b7e6
Brice Bentler <server4001@gmail.com>
2016-09-06 21:48:33 -0700

    Adds createrepo module to create the yum repo during provisioning

---
+++ puppet/modules/createrepo/manifests/init.pp
@@ -0,0 +1,11 @@
+class createrepo {
+  exec { 'create yum repo':
+    command => 'createrepo /var/www/html/packages',
+    timeout => 60,
+    path    => '/usr/bin:/usr/local/bin:/usr/sbin:/usr/local/sbin:/bin',
+    creates => '/var/www/html/packages/repodata/',
+    user    => 'vagrant',
+    group   => 'vagrant',
+    require => File['nginx rpm'],
+  }
+}
